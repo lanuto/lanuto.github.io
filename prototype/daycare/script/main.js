11520eaf6842c0a92e8008dd694e536ae36ff27c
lanuto <168159104+lanuto@users.noreply.github.com>
2025-05-16 10:22:40 +0100

    Add files via upload

--- prototype/daycare/script/main.js
+++ prototype/daycare/script/main.js
@@ -1,29 +1,15 @@
 
+let canvas;
+
 let points = [];
 let pr = [];
-let tints = [];
-let tintIndex = 0;
-
-let visible = true;
-
-let a = 255;
-
-let renderIndex = 0;
-
 let splatterCount;
+let renderIndex = 0;
 
 let nh = 0;
 
-let canvas;
-
 let paint;
-
-// const VISIBLE = 0, FADE_OUT = 1, HIDDEN = 2, FADE_IN = 3;
-// let state = VISIBLE;
-
-// function hideStuff() {
-//     state = FADE_OUT;
-// }
+let whirlWidth;
 
 function preload() {
     // paint = loadImage("../assets/img/paint_splatters.png");
@@ -33,8 +19,6 @@ function preload() {
 function deviceTurned() {
     resizeCanvas(windowWidth, windowHeight);
 }
-
-let whirlWidth;
 
 function setup() {
     // createCanvas(window.width);
@@ -48,24 +32,19 @@ function setup() {
     let offset = 300;
     nh = height * 0.93;
 
-    // splatterCount = (width > 600) ? 50 : 20;
     if (width > 600) {
         splatterCount = 50;
-        // offset = 300;
-        // nh = height * 0.93;
         whirlWidth = 570;
     } else {
         splatterCount = 14;
-        // offset = 150;
-        // nh = height;
         whirlWidth = 380;
     }
     for (let i = 0; i < splatterCount; i++) {
-        // points[i] = createVector(random(0, width), random(0, height));
         points[i] = createVector(random(width / 2 - offset, width / 2 + offset), random(height - nh + nh / 2 - offset, height - nh + nh / 2 + offset));
         pr[i] = createVector(floor(random(0, 8)) * 256, floor(random(0, 2)) * 256);
-        tints[i] = 0;
     }
+
+    background(255, 249, 187);
 }
 
 function mouseReleased() {
@@ -80,53 +59,32 @@ function mouseReleased() {
 
 let index = 0;
 let frame = 0;
-let op = 0.0;
+let opacity = 0.0;
 
 function draw() {
     const logo = document.getElementById("logo");
-    logo.style.opacity = op;
+
+    logo.style.opacity = opacity;
     logo.style.backgroundPosition = (index * -whirlWidth) + "px 0px";
 
     if (frame % 2 == 0 && index < 9) {
         index++;
     }
 
-    if (op < 1.0) {
-        op += 0.02;
+    if (opacity < 1.0) {
+        opacity += 0.05;
     }
-    frame++;
 
-    // clear(); // warning, may remove <img> bg
-    background(255, 249, 187);
-    // image(bg, 0, 0);
-    // rect(mouseX - 25, mouseY - 25, 50, 50);
+    // background(255, 249, 187);
 
-    // switch (state) {
-    // case FADE_OUT:
-    //     if (a > 0) {
-    //         a -= 51;
-    //     } else {
-    //         state == HIDDEN;
-    //     }
-    // case FADE_IN:
-    //     tint(255, a);
-    // case VISIBLE:
-        for (let i = 0; i < renderIndex; i++) {
-            // tint(255, tints[i]);
-            image(paint, points[i].x - 128, points[i].y - 128, 256, 256, pr[i].x, pr[i].y, 256, 256);
-        }
+    for (let i = 0; i < renderIndex; i++) {
+        image(paint, points[i].x - 128, points[i].y - 128, 256, 256, pr[i].x, pr[i].y, 256, 256);
+    }
 
-        if (tints[tintIndex] < 255) {
-            for (let i = tintIndex; i < tintIndex + 10; i++) {
-                tints[i] += 51;
-            }
-        } else
-        if (tintIndex < 19) {
-            tintIndex += 10;
-        }
-    // }
-
+    
     if (renderIndex < splatterCount) {
         renderIndex++;
     }
+
+    frame++;
 }
